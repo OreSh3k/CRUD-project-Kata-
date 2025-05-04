d5eb30865ed5bdddb210ace9ffb7e593b75e8943
Huesos <dino.man21@yandex.ru>
2025-05-04 12:29:17 +0300

    The end

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -18,6 +18,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    //Список всех юзеров
     @GetMapping("/users")
     public String showUsers(Model model) {
         model.addAttribute("users", userService.getAllUsers());
@@ -25,13 +26,13 @@ public class UserController {
     }
 
 
-    // Метод для отображения формы
+    // Форма для добавления
     @GetMapping("/addUserForm")
     public String showAddUserForm() {
         return "addUser";  // Имя HTML страницы (addUserForm.html)
     }
 
-
+    //Добавление юзера
     @PostMapping("/add")
     public String add(
             @RequestParam("name") String name,
@@ -46,6 +47,30 @@ public class UserController {
 
         userService.addUser(user);
 
+        return "redirect:/users";
+    }
+
+    //Форма для редактирования
+    @GetMapping("/editUserForm")
+    public String showEditUserForm(@RequestParam("id") int id, Model model) {
+        User user = userService.findUser(id);
+        model.addAttribute("user", user);
+        return "editUser"; // Это HTML-страница для редактирования
+    }
+
+
+    //Ред. юзера
+    @PostMapping("/updateUser")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+
+    //Удаления юзера
+    @PostMapping("/deleteUser")
+    public String deleteUser(@RequestParam("id") int id) {
+        User user = userService.findUser(id);
+        userService.deleteUserById(user, id);
         return "redirect:/users";
     }
 
