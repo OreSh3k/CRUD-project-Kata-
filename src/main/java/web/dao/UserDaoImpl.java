7159c98926a7e11051b7c8781eaec335d383b4af
User <dino.man21@yandex.ru>
2025-05-13 16:55:13 +0300

    Через SpringBoot

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,11 +1,10 @@
 package web.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import java.util.List;
 
 
@@ -15,28 +14,27 @@ public class UserDaoImpl implements UserDao {
     @PersistenceContext
     private EntityManager em;
 
-    @Transactional
     @Override
     public void addUser(User user) {
         em.persist(user);
 
     }
-    @Transactional
+
     @Override
     public void updateUser(User user) {
         em.merge(user);
     }
-    @Transactional
+
     @Override
     public void deleteUserById(User user, int id) {
         em.remove(em.find(User.class, id));
     }
-    @Transactional
+
     @Override
     public User findUser(int id) {
         return em.find(User.class, id);
     }
-    @Transactional
+
     @Override
     public List<User> getAllUsers() {
         return em.createQuery("select u from User u", User.class).getResultList();
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -2,10 +2,10 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -22,8 +22,7 @@ public class UserServiceImpl implements UserService {
     public void addUser(User user) {
         userDao.addUser(user);
     }
-    @Transactional
-    @Override
+    @Transactional()
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
